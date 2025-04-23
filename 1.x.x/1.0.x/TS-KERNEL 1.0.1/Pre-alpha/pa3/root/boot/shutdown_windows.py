34f3fecf2ea48345d06c812c17f399020df3fd88
Coolis1362 <tadeotherocketbuilder@outlook.com>
2025-04-23 18:43:56 -0400

    Update shutdown_windows.py

--- 1.x.x/1.0.x/TS-KERNEL 1.0.1/Pre-alpha/pa3/root/boot/shutdown_windows.py
+++ 1.x.x/1.0.x/TS-KERNEL 1.0.1/Pre-alpha/pa3/root/boot/shutdown_windows.py
@@ -3,11 +3,4 @@ import keyboard # type: ignore
 import sys
 
 def shutdown_windows():
-    system("echo This Will Shutdown Windows, This is windows Talking. Press Y to shutdown Windows, N to Shutdown TS-KERNEL")
-while True:
-    if keyboard.is_pressed("y"):
-        system("echo Shutting Down Windows")
-        system("shutdown -s")
-
-    elif keyboard.is_pressed("n"):
-        sys.exit(0)
+   system("shutdown -s")
