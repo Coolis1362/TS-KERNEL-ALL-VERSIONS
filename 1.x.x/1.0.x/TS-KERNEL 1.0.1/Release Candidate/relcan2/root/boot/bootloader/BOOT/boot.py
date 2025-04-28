6de15dfd4ba098d507f1e6a59e8348410b6d6d72
Coolis1362 <tadeotherocketbuilder@outlook.com>
2025-04-27 21:30:21 -0400

    Add git command description to boot process output

--- 1.x.x/1.0.x/TS-KERNEL 1.0.1/Release Candidate/relcan2/root/boot/bootloader/BOOT/boot.py
+++ 1.x.x/1.0.x/TS-KERNEL 1.0.1/Release Candidate/relcan2/root/boot/bootloader/BOOT/boot.py
@@ -46,6 +46,7 @@ def boot():
         print("boot - Boot Again")
         print("msnetwork - Start The Microsoft Network")
         print("version - Show the version of the distro and kernel")
+        print("git - Same As The git Command In Other Terminals And Shells")
         time.sleep(0.05)
         print("DONE PROCESS: 20% TIME: SYSTEM READABLE TIME NOT SET HUMAN READABLE TIME NOT SET BOOTED: Commands")
     except Exception as e:
